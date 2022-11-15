74ce00ffbd3967331928cc76333b2da637444f20
Ammar Ljubijankic <Ammar.Ljubijankic@london.gov.uk>
2022-11-15 09:02:49 +0000

    update plot saving function

--- SCRIPTS/SUBSCRIPTS/GLAE_functions_load.R
+++ SCRIPTS/SUBSCRIPTS/GLAE_functions_load.R
@@ -65,9 +65,11 @@ pull_dtpt <- function(dt=NULL,flt=NULL,vr=NULL) {
 # pull_dtpt(dt=paye_nat_long,flt="nationality == 'EU' & region== 'London' & date_day == max(date_day)",vr="employee_count")
 
 # Function to save last chart displayed in PNG and SVG
-save_GLA_plot <- function(plot_name, w=8, h=8) {
-  ggsave(paste0(IMAGES,Sys.Date(),"_",plot_name,".svg"), device = "svg", width = w, height = h, units = "in")
-  ggsave(paste0(IMAGES,Sys.Date(),"_",plot_name,".png"), device = "png", width = w, height = h, units = "in")
+save_GLA_plot <- function(plot_name, path=IMAGES, w=8, h=8,svg_save=TRUE) {
+  if (svg_save==TRUE) ggsave(paste0(path,Sys.Date(),"_",plot_name,".svg"),
+                             device = "svg", width = w, height = h, units = "in")
+  ggsave(paste0(path,Sys.Date(),"_",plot_name,".png"),
+         device = "png", width = w, height = h, units = "in")
 }
 
 # Standard ggplotly settings
